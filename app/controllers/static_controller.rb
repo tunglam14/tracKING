a2f84e980afe0f1347ce1d9d4adc826ce09f622e
Dang Tung Lam <lamdt@familug.org>
2014-03-15 11:47:42 +0700

    integrate with devoops template

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,25 @@
+class StaticController < ApplicationController
+  def index
+    # render layout: false
+  end
+
+  def realtime
+    render layout: false
+  end
+
+  def dashboard
+    render layout: false
+  end
+
+  def visitor
+    render layout: false
+  end
+
+  def page
+    render layout: false
+  end
+
+  def system
+    render layout: false
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tracking::Application.config.secret_key_base = 'a350ff119e953f41888e3e2575bcf782605569ca335c1bbdca9aebf7353e69d139943e5164d9d9f3e5a168e66609b5982c66295eeacbdf0cfd71e1c75bde366c'
